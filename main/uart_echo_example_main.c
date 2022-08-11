2a6f12a1861abd491e534f8dbf4d0eb62be83c49
mengfanyao <mengfan.yao@anviz.com>
2022-08-11 17:17:24 +0800

    0x09

--- main/uart_echo_example_main.c
+++ main/uart_echo_example_main.c
@@ -41,7 +41,7 @@ void uart_init(void) {
 //uart 发送线程
 static void tx_task(void *arg)
 {
-    static uint8_t data[]={0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08};
+    static uint8_t data[]={0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09};
     while (1) {
         //往UART_NUM_1发送"Hello world"
         //uart_write_bytes(UART_NUM_1, "Hello world", strlen("Hello world"));
